25d4656c71575368a0028738631130bb9a5dfa03
Dipayan <dipayan90@yahoo.com>
2018-06-10 21:51:40 -0700

    Incremental update

--- build.sbt
+++ build.sbt
@@ -1,16 +1,18 @@
 name := "spark-data-profiler"
-
 version := "1.0.0"
-
+description := "Profiles Data From Source and generates report"
 scalaVersion := "2.11.8"
 
-sparkVersion := "2.3.0"
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "2.3.0" ,
+  "org.apache.spark" %% "spark-sql" % "2.3.0" ,
+  "org.scalactic" %% "scalactic" % "3.0.5" ,
+  "org.scalatest" %% "scalatest" % "3.0.5" % "test"
+)
 
-sparkComponents += "sql"
-
-sparkComponents ++= Seq("sql","hive")
-
-libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.5"
-libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % "test"
+assemblyMergeStrategy in assembly := {
+  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+  case x => MergeStrategy.first
+}
 
 parallelExecution in Test := false
--- src/main/scala/com/kajjoy/spark/data/profiler/profile/DataFrameProfile.scala
+++ src/main/scala/com/kajjoy/spark/data/profiler/profile/DataFrameProfile.scala
@@ -1,7 +1,9 @@
 package com.kajjoy.spark.data.profiler.profile
 
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
-import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import org.apache.spark.sql.types._
+
+import scala.collection.mutable.ListBuffer
 
 case class DataFrameProfile(df: DataFrame)  {
 
@@ -10,8 +12,23 @@ case class DataFrameProfile(df: DataFrame)  {
   lazy val spark = SparkSession.builder().getOrCreate()
 
   val columnProfiles  : List[ColumnProfile] =
-    for (c <- df.columns.toList)
+    for (c <- getColumnNames(df))
       yield ColumnProfile.ColumnProfileFactory(df,c)
+
+  private def getColumnNames(df: DataFrame) : List[String] = {
+    var columnNames = new ListBuffer[String]()
+    findFields("", df.schema, columnNames)
+    columnNames.toList
+  }
+
+  private def findFields(path: String, dt: DataType, columnNames: ListBuffer[String]): Unit = dt match {
+    case s: StructType =>
+      s.fields.foreach(f => findFields(path + "." + f.name, f.dataType, columnNames))
+    case s: ArrayType => findFields(path, s.elementType, columnNames)
+    case other =>
+      columnNames += path.substring(1)
+  }
+
 
   val header : List[String] = List("Column Name","Record Count", "Unique Values", "Empty Strings" ,"Null Values", "Percent Fill", "Percent Numeric", "Max Length")
 
